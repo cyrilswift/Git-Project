cf70135e4d1cf6e775665ecdd9011f1d4436d50a
Cyril F <cyrilfonrosecoding@gmail.com>
2022-08-20 17:28:04 -0400

    Initial Commit

---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by Cyril Fonrose on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
