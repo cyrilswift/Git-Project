c714eb03c6a255b81720ea433ea283e766d08e44
Cyril F <cyrilfonrosecoding@gmail.com>
2022-08-20 17:56:11 -0400

    Created separated ModalView

---
+++ Git Project/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//  Git Project
+//
+//  Created by Cyril Fonrose on 8/20/22.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is the Modal View ")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
