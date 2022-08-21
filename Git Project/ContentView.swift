e57bdc239e5528be6858495189f94f9240c79777
Cyril F <cyrilfonrosecoding@gmail.com>
2022-08-21 08:58:12 -0400

    added red button color

--- Git Project/ContentView.swift
+++ Git Project/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
         NavigationView {
             Button("Show modal") {
                 self.isPresented = true
-            }.sheet(isPresented: $isPresented) {
+            }.foregroundColor(.red)
+            .sheet(isPresented: $isPresented) {
                 ModalView()
             }
             .navigationTitle("Xcode and Git")
